a20e4f66edae30ae5b7fb2c18695c18a218bab53
talrez11 <tal@way2cy.com>
2015-07-27 09:22:21 +0300

    Added scroll function

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -54,7 +54,20 @@ Site.on_load = function() {
 	.setInterval(5000)
 	.setWrapAround(true);
 
-	Caracal.lightbox = new LightBox('a.image.direct', false, false, true)
+	Caracal.lightbox = new LightBox('a.image.direct', false, false, true);
+
+	$('a[href*=#]').bind('click', function(e) {
+	e.preventDefault(); //prevent the "normal" behaviour which would be a "hard" jump
+
+	var target = $(this).attr("href"); //Get the target
+
+	// perform animated scrolling by getting top-position of target-element and set it as scroll target
+	$('html, body').stop().animate({ scrollTop: $(target).offset().top }, 1200, function() {
+	     location.hash = target;  //attach the hash (#jumptarget) to the pageurl
+	});
+
+	return false;
+   });
 
 };
 
